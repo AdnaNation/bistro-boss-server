0919c4a7111e2c454b58365dabee6045873ab2d7
AdnaNation <azizulislamadnan@gmail.com>
2024-05-23 11:30:12 +0600

    commited

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vksh2ow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri)
 
@@ -54,6 +54,13 @@ async function run() {
           const result = await cartCollection.insertOne(cartItem);
           res.send(result);
         });
+
+        app.delete('/carts/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await cartCollection.deleteOne(query);
+          res.send(result);
+        })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
