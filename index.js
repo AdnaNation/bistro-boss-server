76938303c3cf8c3e7c57e3ffb750820e1dfc4fe5
AdnaNation <azizulislamadnan@gmail.com>
2024-05-31 15:35:54 +0600

    added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -133,11 +134,11 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/menu/:id', async (req, res) => {
+    app.get('/menu/:id', async ( req, res)=>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await menuCollection.findOne(query);
-      res.send(result);
+      res.send(result)
     })
      
     app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
@@ -146,10 +147,38 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+
+      const result = await menuCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+     app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     app.get('/reviews', async(req, res) =>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
     })
+
+
 
         // carts collection
         app.get('/carts', async (req, res) => {
@@ -171,6 +200,24 @@ async function run() {
           const result = await cartCollection.deleteOne(query);
           res.send(result);
         })
+
+
+         // payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
